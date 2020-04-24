07e8d5797f6ef43319074548822d3a488a026ebb
dead end <dead.end@inbox.ru>
2020-04-24 23:40:29 +0200

    update

--- src/hex_game.c
+++ src/hex_game.c
@@ -37,6 +37,7 @@
 
 #include "hg_ship.h"
 #include "hg_obj_area.h"
+#include "hg_viewport.h"
 
 /******************************************************************************
  * The exit callback function resets the terminal and frees the memory. This is
@@ -84,40 +85,43 @@ static void hg_init() {
  *
  *****************************************************************************/
 
-static void print_object(const s_point *hex_idx, const bool highlight) {
+static void print_object(s_viewport *viewport, s_object *obj, const bool highlight) {
 	s_hex_field hf_tmp_bg, hf_tmp_fg;
 
-	const s_object *obj = obj_area_get(hex_idx->row, hex_idx->col);
+	s_point pos_ul;
+	s_viewport_get_ul(viewport, &obj->pos, &pos_ul);
 
 	//
 	// Get the index of the shading (0, 1, 2)
 	//
-	const int color_idx = hex_field_color_idx(hex_idx->row, hex_idx->col);
+	const int color_idx = hex_field_color_idx(obj->pos.row, obj->pos.col);
+
+	log_debug("pos: %d/%d color index: %d", obj->pos.row, obj->pos.col, color_idx);
 
 	switch (obj->obj) {
 
 	case OBJ_NONE:
 
-		log_debug("space: %d/%d", hex_idx->row, hex_idx->col);
+		log_debug("space: %d/%d", obj->pos.row, obj->pos.col);
 
-		space_get_hex_field(hex_idx, color_idx, highlight, &hf_tmp_bg);
+		space_get_hex_field(&obj->pos, color_idx, highlight, &hf_tmp_bg);
 
 		s_marker_add_to_field(obj->marker, color_idx, highlight, &hf_tmp_bg);
 
-		hex_field_print(stdscr, hex_idx, NULL, &hf_tmp_bg);
+		hex_field_print(stdscr, &pos_ul, NULL, &hf_tmp_bg);
 		break;
 
 	case OBJ_SHIP:
 
-		log_debug("ship: %d/%d", hex_idx->row, hex_idx->col);
+		log_debug("ship: %d/%d", obj->pos.row, obj->pos.col);
 
-		space_get_hex_field(hex_idx, color_idx, highlight, &hf_tmp_bg);
+		space_get_hex_field(&obj->pos, color_idx, highlight, &hf_tmp_bg);
 
 		s_marker_add_to_field(obj->marker, color_idx, highlight, &hf_tmp_bg);
 
 		ship_get_hex_field(obj->ship_inst->ship_type, obj->ship_inst->dir, &hf_tmp_fg);
 
-		hex_field_print(stdscr, hex_idx, &hf_tmp_fg, &hf_tmp_bg);
+		hex_field_print(stdscr, &pos_ul, &hf_tmp_fg, &hf_tmp_bg);
 		break;
 
 	default:
@@ -130,33 +134,46 @@ static void print_object(const s_point *hex_idx, const bool highlight) {
  * The function prints the space hex fields of the game.
  *****************************************************************************/
 
-static void print_objects(const s_point *hex_dim) {
+static void print_objects(s_viewport *viewport) {
 	log_debug_str("Print objects");
 
-	s_point hex_idx;
+	s_object *obj;
+	s_point idx_rel, idx_abs;
 
-	for (hex_idx.row = 0; hex_idx.row < hex_dim->row; hex_idx.row++) {
-		for (hex_idx.col = 0; hex_idx.col < hex_dim->col; hex_idx.col++) {
+	if (wclear(stdscr) == ERR) {
+		log_exit_str("Unable to clear window.");
+	}
 
-			print_object(&hex_idx, false);
+	for (idx_rel.row = 0; idx_rel.row < viewport->dim.row; idx_rel.row++) {
+		for (idx_rel.col = 0; idx_rel.col < viewport->dim.col; idx_rel.col++) {
+
+			s_viewport_get_abs(viewport, &idx_rel, &idx_abs);
+
+			obj = obj_area_get(idx_abs.row, idx_abs.col);
+
+			print_object(viewport, obj, false);
 		}
 	}
 }
 
-void reset_marker(const s_point *hex_dim) {
-	log_debug_str("Print objects");
+/******************************************************************************
+ *
+ *****************************************************************************/
+
+static void reset_marker(s_viewport *viewport) {
+	log_debug_str("Resetting marker!");
 
 	s_point hex_idx;
 	s_object *object;
 
-	for (hex_idx.row = 0; hex_idx.row < hex_dim->row; hex_idx.row++) {
-		for (hex_idx.col = 0; hex_idx.col < hex_dim->col; hex_idx.col++) {
+	for (hex_idx.row = 0; hex_idx.row < viewport->max.row; hex_idx.row++) {
+		for (hex_idx.col = 0; hex_idx.col < viewport->max.col; hex_idx.col++) {
 
 			object = obj_area_get(hex_idx.row, hex_idx.col);
 
 			if (object->marker != NULL) {
 				object->marker = NULL;
-				print_object(&hex_idx, false);
+				print_object(viewport, object, false);
 			}
 		}
 	}
@@ -179,7 +196,7 @@ static void set_ship(const int row, const int col, const e_ship_type ship_type, 
  *
  *****************************************************************************/
 
-static void set_marker(s_object *obj_from) {
+static void set_marker(s_viewport *viewport, s_object *obj_from) {
 	s_object *obj_to;
 
 	//
@@ -190,7 +207,7 @@ static void set_marker(s_object *obj_from) {
 	}
 
 	obj_area_set_mv_marker(obj_from, DIR_UNDEF);
-	print_object(&obj_from->pos, true);
+	print_object(viewport, obj_from, true);
 
 	//
 	// Set the marker along the paths.
@@ -205,9 +222,82 @@ static void set_marker(s_object *obj_from) {
 		//
 		obj_to = obj_area_set_mv_marker_path(obj_from, paths[i]);
 		if (obj_to != NULL) {
-			print_object(&obj_to->pos, false);
+			print_object(viewport, obj_to, false);
 		}
 	}
+}
+
+/******************************************************************************
+ *
+ *****************************************************************************/
+
+static s_object* cursor_mv(s_viewport *viewport, s_object *obj_from, s_point *to) {
+
+	log_debug("Moving cursor from: %d/%d to: %d/%d viewport: %d/%d", obj_from->pos.row, obj_from->pos.col, to->row, to->col, viewport->pos.row, viewport->pos.col);
+
+	if (!s_point_inside(&viewport->max, to)) {
+		log_debug("Target outside game: %d/%d", to->row, to->col);
+		return obj_from;
+	}
+
+	if (s_point_same(&obj_from->pos, to)) {
+		log_debug("Source and target are the same: %d/%d", to->row, to->col);
+		return obj_from;
+	}
+
+	s_object *obj_to = obj_area_get(to->row, to->col);
+
+	if (!s_viewport_inside_viewport(viewport, &obj_to->pos)) {
+
+		log_debug("Not inside viewport pos: %d/%d dim: %d/%d", viewport->pos.row, viewport->pos.col, viewport->dim.row, viewport->dim.col);
+
+		if (s_viewport_update(viewport, &obj_to->pos)) {
+			print_objects(viewport);
+		}
+
+		if (!s_viewport_inside_viewport(viewport, &obj_to->pos)) {
+			log_exit_str("Outside");
+		}
+
+		print_object(viewport, obj_to, true);
+		return obj_to;
+	}
+
+	//
+	// Delete old and print the new
+	//
+	if (s_viewport_inside_viewport(viewport, &obj_from->pos)) {
+		print_object(viewport, obj_from, false);
+	}
+	print_object(viewport, obj_to, true);
+
+	return obj_to;
+}
+
+/******************************************************************************
+ * Return next obj_old
+ *****************************************************************************/
+
+static s_object* ship_move(s_viewport *viewport, s_object *obj_from, s_object *obj_to) {
+
+	if (!s_viewport_inside_viewport(viewport, &obj_to->pos)) {
+		return obj_from;
+	}
+
+	if (!obj_area_can_mv_to(obj_to)) {
+		return obj_from;
+	}
+
+	obj_area_mv_ship(obj_from, obj_to, obj_to->marker->marker_move->dir);
+
+	reset_marker(viewport);
+
+	print_object(viewport, obj_from, false);
+	print_object(viewport, obj_to, false);
+
+	set_marker(viewport, obj_to);
+
+	return obj_to;
 }
 
 /******************************************************************************
@@ -215,31 +305,45 @@ static void set_marker(s_object *obj_from) {
  *****************************************************************************/
 
 int main() {
+	s_object *obj_old;
+	s_object *obj_ship;
+
 	s_point hex_idx;
 
-	const s_point hex_max = { .row = 5, .col = 12 };
-
-	s_point hex_idx_old = { .row = -1, .col = -1 };
+	s_viewport viewport;
+	viewport.dim.row = 5;
+	viewport.dim.col = 12;
+	viewport.max.row = 10;
+	viewport.max.col = 24;
+	viewport.pos.row = 0;
+	viewport.pos.col = 0;
 
 	hg_init();
 
-	space_init(&hex_max);
+	space_init(&viewport.max);
 
-	obj_area_init(&hex_max);
+	obj_area_init(&viewport.max);
 
 	ship_field_init();
 
 	s_marker_init();
 
-	s_point ship_point = { .row = 3, .col = 2 };
-
-	set_ship(ship_point.row, ship_point.col, SHIP_TYPE_NORMAL, DIR_NN);
-
 	set_ship(3, 3, SHIP_TYPE_NORMAL, DIR_NE);
 
-	set_marker(obj_area_get(ship_point.row, ship_point.col));
+	obj_ship = obj_area_get(3, 2);
 
-	print_objects(&hex_max);
+	set_ship(obj_ship->pos.row, obj_ship->pos.col, SHIP_TYPE_NORMAL, DIR_NN);
+
+	set_marker(&viewport, obj_ship);
+
+	print_objects(&viewport);
+
+	//
+	// Setting the initial cursor is a little hack, because we need an old
+	// cursor position.
+	//
+	s_point_set(&hex_idx, 0, 0);
+	obj_old = cursor_mv(&viewport, obj_area_get(1, 1), &hex_idx);
 
 	for (;;) {
 		int c = wgetch(stdscr);
@@ -258,49 +362,59 @@ int main() {
 				log_exit_str("Unable to get mouse event!");
 			}
 
-			hex_get_hex_idx(event.y, event.x, &hex_idx, &hex_max);
+			//
+			// From event x/y to relative hex index inside the viewport
+			//
+			hex_get_hex_idx(event.y, event.x, &hex_idx, &viewport.max);
+
+			//
+			// From the relative hex index to the absolute hex index.
+			//
+			s_viewport_get_abs(&viewport, &hex_idx, &hex_idx);
 
 			if (event.bstate & BUTTON1_PRESSED) {
+				obj_ship = ship_move(&viewport, obj_ship, obj_area_get(hex_idx.row, hex_idx.col));
 
-				if (!s_point_inside(&hex_max, &hex_idx)) {
-					continue;
-				}
-
-				s_object *obj_to = obj_area_get(hex_idx.row, hex_idx.col);
-
-				if (!obj_area_can_mv_to(obj_to)) {
-					continue;
-				}
-
-				s_object *obj_from = obj_area_get(ship_point.row, ship_point.col);
-
-				obj_area_mv_ship(obj_from, obj_to, obj_to->marker->marker_move->dir);
-				reset_marker(&hex_max);
-				print_object(&ship_point, false);
-				print_object(&hex_idx, false);
-				set_marker(obj_to);
-				s_point_copy(&ship_point, &hex_idx);
-
-				continue;
+			} else {
+				obj_old = cursor_mv(&viewport, obj_old, &hex_idx);
 			}
 
-			if (!s_point_same(&hex_idx, &hex_idx_old)) {
+		} else {
 
-				//
-				// Delete old
-				//
-				if (hex_idx_old.row >= 0 && hex_idx_old.col >= 0) {
-					print_object(&hex_idx_old, false);
-				}
+			switch (c) {
 
-				//
-				// Print new
-				//
-				if (hex_idx.row >= 0 && hex_idx.col >= 0) {
-					print_object(&hex_idx, true);
-				}
+			case KEY_UP:
+				log_debug_str("arrow up");
+				s_point_set(&hex_idx, obj_old->pos.row - 1, obj_old->pos.col)
+				;
+				obj_old = cursor_mv(&viewport, obj_old, &hex_idx);
+				break;
 
-				s_point_copy(&hex_idx_old, &hex_idx);
+			case KEY_DOWN:
+				log_debug_str("arrow down");
+				s_point_set(&hex_idx, obj_old->pos.row + 1, obj_old->pos.col)
+				;
+				obj_old = cursor_mv(&viewport, obj_old, &hex_idx);
+				break;
+
+			case KEY_LEFT:
+				log_debug_str("arrow left");
+				s_point_set(&hex_idx, obj_old->pos.row, obj_old->pos.col - 1)
+				;
+				obj_old = cursor_mv(&viewport, obj_old, &hex_idx);
+				break;
+
+			case KEY_RIGHT:
+				log_debug_str("arrow right");
+				s_point_set(&hex_idx, obj_old->pos.row, obj_old->pos.col + 1)
+				;
+				obj_old = cursor_mv(&viewport, obj_old, &hex_idx);
+				break;
+
+			case 10:
+				log_debug_str("Enter");
+				obj_ship = ship_move(&viewport, obj_ship, obj_area_get(hex_idx.row, hex_idx.col));
+				break;
 			}
 		}
 	}
