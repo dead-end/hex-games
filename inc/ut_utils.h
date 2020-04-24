7f615a86483528db2effd236e9e5bb441db754e8
dead end <dead.end@inbox.ru>
2020-04-24 23:24:20 +0200

    added function declaration

--- inc/ut_utils.h
+++ inc/ut_utils.h
@@ -37,4 +37,6 @@ void ut_check_short(const short current, const short expected, const char *msg);
 
 void ut_check_s_point(const s_point *current, const s_point *expected, const char *msg);
 
+void ut_check_bool(const bool cur, const bool exp, const char *msg);
+
 #endif /* INC_UT_UTILS_H_ */
